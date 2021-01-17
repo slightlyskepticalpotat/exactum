08de6d301a5d7339726e69b6553c63d731398428
Anthony <31290848+ComputerGenius152@users.noreply.github.com>
2021-01-17 00:13:27 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # website-monitor
-Monitors websites for changes
+
+---
+
+Before using this, you need to create a configuration file named `config.yml`. For each site you want to monitor, add an an entry to the file with the following format. The delay between checks is in seconds, and a pop-up dialog will be shown if you set notify to true.
+```yaml
+job-name:
+    url: https://google.ca
+    delay: 60
+    notify: True
+```
+If you are using the .py file, install Python 3 and the dependencies in README.txt.
---
+++ website_monitor.py
@@ -0,0 +1,62 @@
+import logging
+import queue
+import sys
+import threading
+import time
+import tkinter
+import tkinter.messagebox
+
+import browser_cookie3
+import requests
+import yaml
+
+
+def message_box(message):
+    root = tkinter.Tk()
+    root.withdraw()
+    tkinter.messagebox.showinfo(message[0], message[1])
+    root.destroy()
+
+
+def track_website(job, url, delay, notify):
+    '''Tracks a website and displays a desktop notification.'''
+    last = ""
+    while True:
+        logging.info(f"{job} checked")
+        response = requests.get(url, allow_redirects=True, cookies=yum)
+        if not response.status_code == 200:
+            logging.info(f"{job} unreachable")
+            if notify:
+                messages.put(
+                    ("Website Unreachable", f"{job} is unreachable at {time.strftime('%X %x %Z')}."))
+        elif response.text != last:
+            logging.info(f"{job} changed")
+            if notify:
+                messages.put(
+                    ("Website Changed", f"{job} has changed at {time.strftime('%X %x %Z')}."))
+            last = response.text
+        time.sleep(delay)
+
+
+with open("config.yml") as config_file:
+    config = yaml.full_load(config_file)
+if not config:
+    message_box(("Notification", "Configuration file missing; exiting"))
+    sys.exit()
+yum = browser_cookie3.load()
+if not yum:
+    message_box(
+        ("Notification", "No browser cookies detected; logins will not work"))
+logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO,
+                    datefmt="%H:%M:%S", filename="website_monitor.log")
+                    
+messages = queue.Queue(maxsize=0)
+for job in config:
+    logging.info(f"Starting monitoring job: {config[job]}")
+    threading.Thread(target=track_website, args=(job,
+                                                 config[job]["url"], config[job]["delay"], config[job]["notify"])).start()
+while True:
+    while messages:
+        latest = messages.get()
+        message_box(latest)
+    time.sleep(10)
